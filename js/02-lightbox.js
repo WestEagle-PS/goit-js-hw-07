231df8d9e7bc8a42a1fb7707179e39c08b037acf
Anton Orlov <anton_orlov@ukr.net>
2023-01-11 16:10:15 +0200

    2nd gallery update

---
+++ js/02-lightbox.js
@@ -0,0 +1,25 @@
+import { galleryItems } from './gallery-items.js';
+
+const galleryEl = document.querySelector('.gallery');
+
+const markupGallery = galleryItems
+  .map(
+    image => `<a class="gallery__item" href="${image.original}">
+  <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>`,
+  )
+  .join('');
+
+galleryEl.insertAdjacentHTML('afterbegin', markupGallery);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  scrollZoom: false,
+  navText: ['←', '→'],
+  close: false,
+  showCounter: false,
+  swipeTolerance: 120,
+  widthRatio: 0.8,
+  heightRatio: 1,
+  fadeSpeed: 200,
+});
